339de704ca247af38b81b072cc2c255667f27624
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-16 18:20:33 +0300

    "Updated consumer.py, models.py, and chat.html to add online user tracking and display online count in chat window"

--- a_rchat/consumer.py
+++ a_rchat/consumer.py
@@ -14,12 +14,21 @@ class ChatroomConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name,self.channel_name
         )
-        self.accept()
         
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+        
+        self.accept()
+    
     def disconnect(self,close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
     
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -47,4 +56,17 @@ class ChatroomConsumer(WebsocketConsumer):
             'user':self.user,
         }
         html=render_to_string("a_rchat/partials/chat_message_p.html",context=context)
+        self.send(text_data=html)
+    
+    def update_online_count(self):
+        online_count=self.chatroom.users_online.count()
+        event={
+            'type':'online_count_handler',
+            'online':online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+    
+    def online_count_handler(self,event):
+        online_count=event['online_count']
+        html=render_to_string("a_rchat/partials/online_count.html",{'online_count':online_count})
         self.send(text_data=html)
