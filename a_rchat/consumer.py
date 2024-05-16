26302ea59a62e8b80f8c048dbf9a1a857d6945c6
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-16 15:59:26 +0300

    "Updated consumer.py, chat.html, and urls.py files; added WebSocket functionality and chat message handling; modified database schema."

--- a_rchat/consumer.py
+++ a_rchat/consumer.py
@@ -1,5 +1,28 @@
 from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404  
+from django.template.loader import render_to_string
+from .models import *
+import json
 
 class ChatroomConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
+        self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
+        self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
         self.accept()
+    
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        body = text_data_json['body']
+        
+        message = GroupMessage.objects.create(
+            body=body,
+            author=self.user,
+            group=self.chatroom
+        )
+        context={
+            'message':message,
+            'user':self.user,
+        }
+        html=render_to_string("a_rchat/partials/chat_message_p.html",context=context)
+        self.send(text_data=html)
