83fdee388b8db2e83608f0902ee8ef7b59f7ca33
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-16 16:21:28 +0300

    "Added Channels support for real-time chat, updated settings and consumer files, modified chat HTML template, and removed JavaScript code from template"

--- a_core/settings.py
+++ a_core/settings.py
@@ -80,7 +80,11 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'a_core.wsgi.application'
 ASGI_APPLICATION = 'a_core.asgi.application'
 
-
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND':"channels.layers.InMemoryChannelLayer",
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- a_rchat/consumer.py
+++ a_rchat/consumer.py
@@ -1,6 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404  
 from django.template.loader import render_to_string
+from asgiref.sync import async_to_sync
 from .models import *
 import json
 
@@ -9,7 +10,16 @@ class ChatroomConsumer(WebsocketConsumer):
         self.user = self.scope['user']
         self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
         self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name,self.channel_name
+        )
         self.accept()
+        
+    def disconnect(self,close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
     
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -20,6 +30,18 @@ class ChatroomConsumer(WebsocketConsumer):
             author=self.user,
             group=self.chatroom
         )
+        event={
+            'type':'message_handler',
+            'message_id':message.id
+        }
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name,event
+        )
+    
+    def message_handler(self,event):
+        message_id=event['message_id']
+        message=GroupMessage.objects.get(id=message_id)
         context={
             'message':message,
             'user':self.user,
