adcf530a7fb378855c4ad81b394e8973c98f1b89
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-23 14:13:34 +0300

    "Added chatroom delete, leave, and file upload views"

--- a_rchat/views.py
+++ a_rchat/views.py
@@ -118,3 +118,50 @@ def chatroom_edit_view(request, chatroom_name):
         'chat_group' : chat_group
     }   
     return render(request, 'a_rchat/chatroom_edit.html', context) 
+
+
+@login_required
+def chatroom_delete_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+    
+    if request.method == "POST":
+        chat_group.delete()
+        messages.success(request, 'Chatroom deleted')
+        return redirect('home')
+    
+    return render(request, 'a_rtchat/chatroom_delete.html', {'chat_group':chat_group})
+
+
+@login_required
+def chatroom_leave_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user not in chat_group.members.all():
+        raise Http404()
+    
+    if request.method == "POST":
+        chat_group.members.remove(request.user)
+        messages.success(request, 'You left the Chat')
+        return redirect('home')
+    
+    
+def chat_file_upload(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    
+    if request.htmx and request.FILES:
+        file = request.FILES['file']
+        message = GroupMessage.objects.create(
+            file = file,
+            author = request.user, 
+            group = chat_group,
+        )
+        channel_layer = get_channel_layer()
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(channel_layer.group_send)(
+            chatroom_name, event
+        )
+    return HttpResponse()
